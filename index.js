93de565416f595532ccee7e5d6292b432239f83d
paulos-t <65367687+paulos-t@users.noreply.github.com>
2021-07-22 14:24:38 -0700

    Done

--- index.js
+++ index.js
@@ -2,5 +2,46 @@ document.addEventListener("DOMContentLoaded", function() {
   // initializes the board
   init();
 
-  // ADD CODE HERE!
+  // Store the commands
+  const movesContainer = document.querySelector("#moves-container")
+
+  document.addEventListener("keydown", arrowKey)
+
+  function arrowKey(event) {
+    if (event.key === "ArrowLeft") {
+      const leftArrow = document.createElement("li")
+      leftArrow.textContent = "Left"
+      movesContainer.append(leftArrow)
+    } else if (event.key === "ArrowUp") {
+      const upArrow = document.createElement("li")
+      upArrow.textContent = "Up"
+      movesContainer.append(upArrow)
+    } else if (event.key === "ArrowRight") {
+      const rightArrow = document.createElement("li")
+      rightArrow.textContent = "Right"
+      movesContainer.append(rightArrow)
+    } else if (event.key === "ArrowDown") {
+      const downArrow = document.createElement("li")
+      downArrow.textContent = "Down"
+      movesContainer.append(downArrow)
+    }
+  }
+
+  // Move the robot
+  const moveButton = document.querySelector("#move-button")
+
+  moveButton.addEventListener("click", moveRobot)
+
+  function moveRobot() {
+    const newDirection = document.querySelector("li")
+    move(newDirection.textContent.toLowerCase())
+    newDirection.remove()
+  }
+
+  // Delete a move
+  movesContainer.addEventListener("click", moveDeleter)
+
+  function moveDeleter(event) {
+    event.target.remove()
+  }
 });
